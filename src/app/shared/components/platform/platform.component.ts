5b30eed3c10ee8ba863cf404ef02cd162024df44
Léo <rolland.leo@orange.fr>
2019-12-14 17:33:36 +0100

    Ajout composant platform

---
+++ src/app/shared/components/platform/platform.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../../../core/services';
+
+@Component({
+  selector: 'app-platform',
+  templateUrl: './platform.component.html',
+  styleUrls: ['./platform.component.scss']
+})
+export class PlatformComponent implements OnInit {
+
+  constructor(private _es: ElectronService) { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 
-import { PageNotFoundComponent } from './components/';
+import { PageNotFoundComponent, PlatformComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective],
+  declarations: [PageNotFoundComponent, WebviewDirective, PlatformComponent],
   imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  exports: [TranslateModule, WebviewDirective, FormsModule, PlatformComponent]
 })
 export class SharedModule {}
