a6d733bfca81b64f65ad4247a9e310479f2eda17
Léo <rolland.leo@orange.fr>
2020-02-06 20:04:28 +0100

    Réimplemented with ngzorro UIkit

--- src/app/home/note-tabs/note-tabs.component.ts
+++ src/app/home/note-tabs/note-tabs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { TabsManagerService } from '../../services/tabsManager/tabs-manager.service';
 import { NoteTab } from '../../services/tabsManager/NoteTab';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-note-tabs',
@@ -10,27 +10,39 @@ import { Subscription, Observable } from 'rxjs';
 })
 export class NoteTabsComponent implements OnInit, OnDestroy {
 
-  constructor(private _tmS: TabsManagerService, private cd: ChangeDetectorRef) { }
+  // private cd: ChangeDetectorRef
+  constructor(private _tmS: TabsManagerService) { }
 
-  tabs: NoteTab[]
+  tabs: NoteTab[] = []
 
+  selectedIndex = 0;
   /**
    * Stores every subscriptions in this component, these will be unsubscribed on destroy
    */
   private subs: Subscription[] = []
 
   ngOnInit() {
-    this.subs.push(this._tmS.openedNotesObservable.subscribe(noteTabs => {
-      this.tabs = noteTabs
-    }))
+    this.subs.push(
+      this._tmS.openedNotesObservable.subscribe(noteTabs => {
+        const ancientTabsCount = this.tabs.length
+        this.tabs = noteTabs
+        // S'il s'agit d'un nouvel onglet, focus dessus
+        if (this.tabs.length > ancientTabsCount) this.selectedIndex = this.tabs.length - 1
+      }),
+      this._tmS.alreadyOpenedObservable.subscribe(noteTab => {
+        // L'utilisateur à probablement demandé au service d'ouvrir une note déjà ouverte
+        // Alors on switch vers cet onglet
+        this.selectedIndex = this.tabs.findIndex(tab=>tab === noteTab)
+      })
+    )
   }
   ngOnDestroy() {
     this.subs.forEach(s=>s.unsubscribe())
   }
-  handleClose(tab: any) {
-    this._tmS.close(this.tabs[tab.index].savedNote.meta.uuid)
+  handleClose(tab: NoteTab) {
+    this._tmS.close(tab.savedNote.meta.uuid)
     // Call Angular to check for updates in children to avoid ExpressionChangedAfterItHasBeenCheckedError
-    this.cd.detectChanges() 
+    // this.cd.detectChanges() 
   }
 
 }
