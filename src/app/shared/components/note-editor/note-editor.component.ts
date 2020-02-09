fe6bcae46258b22164f74aa8a0c613fead35edc9
Léo <rolland.leo@orange.fr>
2020-02-09 23:44:40 +0100

    cleaned note editor

--- src/app/shared/components/note-editor/note-editor.component.ts
+++ src/app/shared/components/note-editor/note-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ViewContainerRef, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ViewContainerRef, ElementRef, AfterViewInit } from '@angular/core';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header'; 
 import List from '@editorjs/list'; 
@@ -14,23 +14,14 @@ export class NoteEditorComponent implements AfterViewInit {
   @Input() 
   note: string
 
-  constructor(private rd: Renderer2) { }
+  constructor() { }
 
   ngAfterViewInit() {
-    console.log(this.rd); 
     const editor = new EditorJS({
-      /**
-       * Id of Element that should contain Editor instance
-       */
       holder: this.el.nativeElement,
-        /** 
-       * Available Tools list. 
-       * Pass Tool's class or Settings object for each Tool you want to use 
-       */ 
       tools: { 
         header: Header, 
         list: List,
-        // image: SimpleImage
       }, 
       
     });
