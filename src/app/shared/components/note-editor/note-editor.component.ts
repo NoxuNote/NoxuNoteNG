db8496047bfbe68a25e3274b42eeb5e717c4a5a0
Léo <rolland.leo@orange.fr>
2020-02-10 21:36:03 +0100

    Added configuration opts

--- src/app/shared/components/note-editor/note-editor.component.ts
+++ src/app/shared/components/note-editor/note-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ViewChild, ViewContainerRef, ElementRef, AfterViewInit } from '@angular/core';
 import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header'; 
-import List from '@editorjs/list'; 
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
 
 @Component({
   selector: 'app-note-editor',
@@ -9,22 +9,43 @@ import List from '@editorjs/list';
   styleUrls: ['./note-editor.component.scss']
 })
 export class NoteEditorComponent implements AfterViewInit {
-  @ViewChild('editorJs') el:ElementRef;
+  @ViewChild('editorJs') el: ElementRef;
 
-  @Input() 
+  @Input()
   note: string
+
+  /**
+   * Editor.js instance
+   */
+  editor: EditorJS
 
   constructor() { }
 
   ngAfterViewInit() {
-    const editor = new EditorJS({
+    this.editor = new EditorJS({
       holder: this.el.nativeElement,
-      tools: { 
-        header: Header, 
+      autofocus: true,
+      placeholder: "Entrez du texte",
+      tools: {
+        header: Header,
         list: List,
-      }, 
-      
+      },
+      /**
+      * onReady callback
+      */
+      onReady: () => {
+        console.log('Editor.js is ready to work!')
+      },
+      onChange: () => {
+        console.log('onChange')
+      }
     });
+  }
+
+  save() {
+    this.editor.save().then(outputData => {
+      console.log(outputData);
+    }).catch(console.error)
   }
 
 }
