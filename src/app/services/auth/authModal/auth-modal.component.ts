0bbef1b53dda2834d0256a30e7826a7a7c289e57
Léo <rolland.leo@orange.fr>
2021-05-25 13:32:19 +0200

    fix : dynamic login URL between prod and dev mode

--- src/app/services/auth/authModal/auth-modal.component.ts
+++ src/app/services/auth/authModal/auth-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NzModalRef } from 'ng-zorro-antd';
+import { isDevMode } from '@angular/core';
+import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-auth-modal',
@@ -8,9 +11,14 @@ import { NzModalRef } from 'ng-zorro-antd';
 })
 export class AuthModalComponent implements OnInit {
 
-  constructor(private modal: NzModalRef) {}
+  loginUrl: SafeResourceUrl;
 
-  ngOnInit() { }
+  constructor(private modal: NzModalRef, private sanitizer:DomSanitizer) {}
+
+  ngOnInit() {
+    let rawUrl = isDevMode() ? 'http://127.0.0.1:4455/' : 'https://cloud.noxunote.fr/'
+    this.loginUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+  }
 
   /**
    * Destroys the modal
