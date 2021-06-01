46e659357a01c27803ec3602371615aa13908381
Léo <rolland.leo@orange.fr>
2021-06-01 11:12:08 +0200

    add: cache, pull and push OK on cloud metadata

--- src/app/services/io/cloud/cached-cloud-note-api.service.ts
+++ src/app/services/io/cloud/cached-cloud-note-api.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, OnInit } from "@angular/core";
-import { BehaviorSubject, interval, Observable, Subject } from "rxjs";
-import { distinctUntilChanged, first, tap } from "rxjs/operators";
+import { BehaviorSubject, from, interval, Observable, Subject } from "rxjs";
+import { distinctUntilChanged, first, map, tap } from "rxjs/operators";
 import { Note } from "../../../types/Note";
 import { NoteMetadata } from "../../../types/NoteMetadata";
 import { INoteAPI } from "../INoteAPI";
 import { CachedCloudDatabase } from "./CachedCloudDatabase";
 import { CloudNoteAPIService } from "./cloud-note-api.service";
+import _ from "lodash"
 
 @Injectable({
   providedIn: 'root'
@@ -14,18 +15,21 @@ export class CachedCloudNoteAPIService implements INoteAPI {
 
   db = new CachedCloudDatabase()
 
-  metadatasSubject = new BehaviorSubject<NoteMetadata[]>([])
+  private metadatasSubject = new BehaviorSubject<NoteMetadata[]>([])
 
   constructor(private cloudAPIService: CloudNoteAPIService) { 
     this.db.metadatas.toArray().then(notes => this.metadatasSubject.next(notes))
-    interval(10*1000).subscribe(() => this.pullMetadatas())
+    // interval(10*1000).subscribe(() => this.pullMetadatas())
     this.pullMetadatas()
+    // When browser is back online, push cached data
+    window.addEventListener("online", () => {
+      this.pushMetadatas()
+    })
   }
 
   getListNotes(): Observable<NoteMetadata[]> {
-    return this.metadatasSubject.asObservable().pipe(
-      distinctUntilChanged((x, y) => JSON.stringify(x) == JSON.stringify(y))
-    )
+    // TODO - Optimiser en utilisant distinctUntilChange
+    return this.metadatasSubject.asObservable()
   }
 
   getNote(uuid: string): Observable<Note> {
@@ -37,17 +41,46 @@ export class CachedCloudNoteAPIService implements INoteAPI {
   createNote(title?: string): Observable<NoteMetadata> {
     return this.cloudAPIService.createNote(title)
   }
+
   saveMetadata(newMetadata: NoteMetadata): Observable<NoteMetadata> {
-    return this.cloudAPIService.saveMetadata(newMetadata)
+    if (navigator.onLine) {
+      return this.cloudAPIService.saveMetadata(newMetadata)
+      .pipe(tap(newMetadata => this.updateCachedMetadata(newMetadata)))
+    } else {
+      return this.updateCachedMetadata(newMetadata)
+    }
   }
+
   removeNote(n: NoteMetadata): Observable<void> {
     return this.cloudAPIService.removeNote(n)
   }
 
+  private updateCachedMetadata(newMetadata: NoteMetadata): Observable<NoteMetadata> {
+     // Update subject
+     let oldList = this.metadatasSubject.getValue()
+     let oldMeta = oldList.find(meta => meta.uuid == newMetadata.uuid) 
+     Object.assign(oldMeta, newMetadata)
+     this.metadatasSubject.next(oldList)
+     // Update cache
+     return from(this.db.metadatas.put(newMetadata, newMetadata.uuid)).pipe(map(() => newMetadata))
+  }
+
   private pullMetadatas() {
+    if (!navigator.onLine) return
     this.cloudAPIService.getListNotes().subscribe(notes => {
+      // Update cache
       this.db.metadatas.bulkAdd(notes)
+      // Update subject
       this.metadatasSubject.next(notes)
+    })
+  }
+
+  private pushMetadatas() {
+    if (!navigator.onLine) return 
+    this.cloudAPIService.getListNotes().subscribe(serverMetas => {
+      const cachedMetas = this.metadatasSubject.getValue()
+      const delta = _.differenceWith(cachedMetas, serverMetas, _.isEqual)
+      delta.forEach(d => this.cloudAPIService.saveMetadata(d).subscribe())
     })
   }
 
